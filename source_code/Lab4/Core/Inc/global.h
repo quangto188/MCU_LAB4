3245d2d24035435b526cc2ea173706f85bb1f7ec
quangto188 <todiuquang123@gmail.com>
2023-12-05 16:16:25 +0700

    lab4

---
+++ source_code/Lab4/Core/Inc/global.h
@@ -0,0 +1,14 @@
+/*
+ * global.h
+ *
+ *  Created on: Nov 30, 2023
+ *      Author: quang
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+extern int timestamp;
+extern int min_index;
+extern int min_delay;
+#endif /* INC_GLOBAL_H_ */
---
+++ source_code/Lab4/Core/Src/global.c
@@ -0,0 +1,12 @@
+/*
+ * global.c
+ *
+ *  Created on: Nov 30, 2023
+ *      Author: quang
+ */
+
+#include "global.h"
+
+int timestamp = 0;
+int min_index = 0;
+int min_delay = 0;
